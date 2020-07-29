6bb1cf62a8f3c88054ed8b554d01089da409d88c
Jakob <43860560+jakbellamy@users.noreply.github.com>
2020-07-29 13:43:26 -0400

    oh my god i neeeded to refactor so bad

--- src/views/dashboard/Summes-Live/index.js
+++ src/views/dashboard/Summes-Live/index.js
@@ -5,6 +5,7 @@ import AccountBio from './AccountBio';
 import CompareLineChart from './CompareLineChart';
 import Header from './Header';
 import Overview from './Overview';
+const methods = require('./methods')
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -20,94 +21,6 @@ const useStyles = makeStyles((theme) => ({
     }
   }
 }));
-
-const getYear = (report) => {
-  let date = report['date']
-  return Number(date.split('-')[0])
-}
-
-const strToDate = (date) => {
-  return Number(date.split('-').join(''))
-};
-
-const getCurrentReport = (reports) => {
-  let sorted = reports.sort(function (a, b) {
-    return strToDate(b.date) - strToDate(a.date);
-  })
-  return sorted[0]
-}
-
-const getReportAttr = (reports, attr) => {
-  if(reports.ly === undefined){
-    return [{ly: [], y: []}]
-  } else {
-    let ly = reports.ly.map(report => {
-      return report[attr]
-    })
-    let y = reports.y.map(report => {
-      return report[attr]
-    })
-    return {y: y, ly: ly}
-  }
-}
-
-const getReportPercent = (reports, attr) => {
-  if(reports === undefined){
-    return [{ly: [], y: []}]
-  } else {
-    let ly = reports.ly.map(report => {
-      return Math.round(report[attr] * 100)
-    })
-    let y = reports.y.map(report => {
-      return Math.round(report[attr] * 100)
-    })
-    return {y: y, ly: ly}
-  }
-}
-
-const accuYtd = (yearArr, current=false) => {
-  if(yearArr === undefined){
-    if(current){
-      return {office_volume: 0, supreme_volume: 0, office_units: 0, supreme_units: 0}
-    } else {
-      return []
-    }
-  } else {
-    yearArr = yearArr.sort(function (a, b) {
-      return strToDate(a.date) - strToDate(b.date);
-    })
-
-    let ytd = []
-    let acc = {office_volume: 0, supreme_volume: 0, office_units: 0, supreme_units: 0}
-
-    for (var i = 0; i < yearArr.length; i++) {
-      let month = yearArr[i]
-
-      acc.office_volume += month.office_volume
-      acc.supreme_volume += month.supreme_volumes
-      acc.office_units += month.office_units
-      acc.supreme_units += month.supreme_units
-
-      let month_ytd = {
-        date: month.date,
-        account: month.account,
-        office_volume: acc.office_volume,
-        supreme_volume: acc.supreme_volume,
-        office_units: acc.office_units,
-        supreme_units: acc.supreme_units,
-        market_share_volume: acc.supreme_volume / acc.office_volume,
-        market_share_units: acc.supreme_units / acc.office_volume
-      }
-      ytd.push(month_ytd)
-    }
-
-    if(current){
-      return ytd.reverse()[0]
-    } else {
-      return ytd.reverse()
-    }
-  }
-}
 
 function DashboardAlternativeView() {
   const [accounts, setAccounts] = useState([])
@@ -138,11 +51,11 @@ function DashboardAlternativeView() {
     let reports = marketReports.filter(report => report.account === account.id)
 
     let ly = reports.filter(report => {
-      return getYear(report) === now - 1
+      return methods.getYear(report) === now - 1
     })
 
     let y = reports.filter(report => {
-      return getYear(report) === now
+      return methods.getYear(report) === now
     })
 
     return {y: y, ly: ly}
@@ -151,10 +64,10 @@ function DashboardAlternativeView() {
   const buildData = (reports) => {
     let raw = coerceReports(reports)
     let ytd = {
-      ly: accuYtd(raw.ly),
-      y: accuYtd(raw.y)
+      ly: methods.accuYtd(raw.ly),
+      y: methods.accuYtd(raw.y)
     }
-    let current = getCurrentReport(ytd.ly)
+    let current = methods.getCurrentReport(ytd.ly)
     return {raw: raw, ytd: ytd, current: current}
   }
 
@@ -170,10 +83,10 @@ function DashboardAlternativeView() {
         <Header accounts={accounts} selectedAccount={selectedAccount} setSelectedAccount={handleAccountSelection}/>
         <Grid container spacing={3}>
           <Grid item xs={12}>
-            <Overview report={accuYtd(selectedReports.ly, true)}/>
+            <Overview report={methods.accuYtd(selectedReports.ly, true)}/>
           </Grid>
           <Grid item lg={8} xl={9} xs={12}>
-            <CompareLineChart selectedAccount={selectedAccount} reports={getReportPercent(selectedReports.ytd, 'market_share_volume')}/>
+            <CompareLineChart selectedAccount={selectedAccount} reports={methods.getReportPercent(selectedReports.ytd, 'market_share_volume')}/>
           </Grid>
           <Grid item lg={4} xl={3} xs={12}>
             <AccountBio />
