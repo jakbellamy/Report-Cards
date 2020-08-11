f04b6967a5dbb22a274106eed6007021ab3f88b1
Jakob <43860560+jakbellamy@users.noreply.github.com>
2020-08-11 14:10:05 -0400

    added volume and units headers

--- src/views/dashboard/Summes-Live/overview/index.js
+++ src/views/dashboard/Summes-Live/overview/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Volume from './volume'
 import Units from './units'
-import { Box, Grid, Card, CardHeader, Divider, makeStyles, Button } from '@material-ui/core';
+import { Box, Grid, Card, CardHeader, Divider, makeStyles, Button, Typography} from '@material-ui/core';
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     marginBottom: theme.spacing(1)
-  },
+  }
 }));
 
 function Overview(props, { className, ...rest }) {
@@ -48,10 +48,16 @@ function Overview(props, { className, ...rest }) {
   return (
     <Grid container spacing={2} justify-self={'stretch'} className={classes.root}>
       <Grid item xs={6} xl={2} spacing={1}>
-        <Volume y={y} ly={ly} insertCommas={insertCommas} calcChange={calcChange} />
+        <Box>
+          <Typography variant={"subtitle1"}>YTD Volume</Typography>
+          <Volume y={y} ly={ly} insertCommas={insertCommas} calcChange={calcChange} />
+        </Box>
       </Grid>
       <Grid item xs={6} xl={2} spacing={1}>
-        <Units y={y} ly={ly} insertCommas={insertCommas} calcChange={calcChange} />
+        <Box>
+          <Typography variant={"subtitle1"}>YTD Units</Typography>
+          <Units y={y} ly={ly} insertCommas={insertCommas} calcChange={calcChange} />
+        </Box>
       </Grid>
     </Grid>
   )
