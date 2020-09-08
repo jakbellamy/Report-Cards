c28f1d82996ccd1aa60af9f35264f4121e894268
Jakob <43860560+jakbellamy@users.noreply.github.com>
2020-09-08 17:11:00 -0400

    leads are finished for now

--- src/views/dashboard/AccountManager/src/Leads/Leads.js
+++ src/views/dashboard/AccountManager/src/Leads/Leads.js
@@ -21,6 +21,14 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const accumulateLeads = (arr) => {
+  let counter = 0
+  for(let month of arr){
+    counter += month.leads
+  }
+  return counter
+}
+
 function Leads(props, { className, ...rest }) {
   const classes = useStyles();
 
@@ -28,16 +36,16 @@ function Leads(props, { className, ...rest }) {
   let quarterLeads = props.leads.filter(lead => lead.date >= "2020-07-01")
 
   let monthCount = monthLeads.length > 0 ? monthLeads[0]['leads'] : 0
-  let quarterCount = quarterLeads.length > 0 ? quarterLeads.reduce((prev, cur) => prev + cur.leads) : 0
-  // console.log(quarterCount)
+  let quarterCount = quarterLeads.length > 0 ? accumulateLeads(quarterLeads) : 0
 
   let title = props.account.monthly_customer_min ? `Lead Quota: ${props.account.monthly_customer_min}/Month` : 'Lead Quota'
   let month = props.account.monthly_customer_min ? `${monthCount} Out of ${props.account.monthly_customer_min}` : ''
-  let quarter = props.account.monthly_customer_min ? `${monthCount} Out of ${Number(props.account.monthly_customer_min) * 4}` : ''
+  let quarter = props.account.monthly_customer_min ? `${quarterCount} Out of ${Number(props.account.monthly_customer_min) * 4}` : ''
   let ytd = props.account.monthly_customer_min ? `000 Out of ${Number(props.account.monthly_customer_min) * 7}` : ''
 
   let monthCheck = monthCount > props.account.monthly_customer_min ? <CheckCircleIcon style={{color: "#25e565"}} /> : <NotInterestedIcon style={{color: "#e5252b"}}/>
-  // let quarterCheck =
+  let quarterCheck = quarterCount > (props.account.monthly_customer_min * 2) ? <CheckCircleIcon style={{color: "#25e565"}} /> : <NotInterestedIcon style={{color: "#e5252b"}}/>
+
   return (
     <Card
       className={clsx(classes.root, className)}
@@ -70,7 +78,7 @@ function Leads(props, { className, ...rest }) {
 
           <Grid item container spacing={3}>
             <Grid item xs={2}>
-              {props.account.monthly_customer_min ? monthCheck : null}
+              {props.account.monthly_customer_min ? quarterCheck : null}
             </Grid>
             <Grid item xs={4}>
               <Typography variant="subtitle1" color="textPrimary">
