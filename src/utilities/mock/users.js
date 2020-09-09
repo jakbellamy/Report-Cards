b67308962aecf0550bd8f894b4a0b180baaad8dd
Jakob <43860560+jakbellamy@users.noreply.github.com>
2020-09-09 10:28:21 -0400

    added logins. Changed everyone to admin

--- src/utilities/mock/users.js
+++ src/utilities/mock/users.js
@@ -32,35 +32,91 @@ module.exports = [
     lastName: 'Flood',
   },
   {
-    id: '6',
-    role: 'smgr',
+    id: 'bqcu4',
+    role: 'admin',
     email: 'Nick.Yeargin@supremelending.com',
     password: 'ny6smgr',
     firstName: 'Nick',
     lastName: 'Yeargin'
   },
   {
-    id: '4',
-    role: 'smgr',
+    id: 'b164bd',
+    role: 'admin',
     email: 'John.Ragland@supremelending.com', // not his real email
     password: 'jr4smgr',
     firstName: 'John',
     lastName: 'Ragland'
   },
   {
-    id: '8',
-    role: 'smgr',
+    id: 'tuxb35',
+    role: 'admin',
     email: 'Brian.Messer@supremelending.com',
     password: 'bm8smgr',
     firstName: 'Brian',
     lastName: 'Messer'
   },
   {
-    id: '9',
-    role: 'smgr',
+    id: 'vs7d',
+    role: 'admin',
     email: 'Richard.Lester@supremelending.com',
     password: 'rl9smgr',
-    firstName: 'Brian',
-    lastName: 'Messer'
+    firstName: 'Richard',
+    lastName: 'Lester'
   },
+  {
+    id: 'inw8c',
+    role: 'admin',
+    email: 'Amir.Davari@supremelending.com', // Not his real Email
+    password: 'ad12smgr',
+    firstName: 'Amir',
+    lastName: 'Davari'
+  },
+  {
+    id: 'qvnr61',
+    role: 'admin',
+    email: 'Scott.Mackie@supremelending.com',
+    password: 'sm91smgr',
+    firstName: 'Scott',
+    lastName: 'Mackie'
+  },
+  {
+    id: '83hsyc7',
+    role: 'admin',
+    email: 'Keith.Johnson@supremelending.com',
+    password: 'kj76smgr',
+    firstName: 'Keith',
+    lastName: 'Johnson'
+  },
+  {
+    id: '74hvs6',
+    role: 'admin',
+    email: 'Robert.Wills@supremelending.com',
+    password: 'rw24smgr',
+    firstName: 'Robert',
+    lastName: 'Wills'
+  },
+  {
+    id: 'peb63m',
+    role: 'admin',
+    email: 'Tom.Flood@supremelending.com',
+    password: 'tf16smgr',
+    firstName: 'Tom',
+    lastName: 'Flood',
+  },
+  {
+    id: 'y254nw',
+    role: 'admin',
+    email: 'Jeffrey.Sachs@supremelending.com',
+    password: 'js81smgr',
+    firstName: 'Jeffrey',
+    lastName: 'Sachs'
+  },
+  {
+    id: 'nsdfuv72hvi',
+    role: 'admin',
+    email: 'Patrick.Flood@supremelending.com',
+    password: 'adminpflood',
+    firstName: 'Pat',
+    lastName: 'Flood'
+  }
 ]
