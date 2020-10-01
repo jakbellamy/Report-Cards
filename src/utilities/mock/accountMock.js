e959e2e14dae3bf200fb0c2252c159a16905bcb4
Jakob <43860560+jakbellamy@users.noreply.github.com>
2020-10-01 11:59:58 -0400

    hiding users?

--- src/utilities/mock/accountMock.js
+++ src/utilities/mock/accountMock.js
@@ -9,145 +9,22 @@ const db = {
 };
 
 const users = [
-  {
-    id: '1',
-    role: 'admin',
-    email: 'jakbellamy@gmail.com',
-    password: 'admin',
-    firstName: 'Jakob',
-    lastName: 'Bellamy',
-  },
-  {
-    id: '00178682h1',
-    role: 'admin',
-    email: 'jmvolpe@nc.rr.com',
-    password: 'adminvolpe',
-    firstName: 'Jeanne',
-    lastName: 'Volpe',
-  },
-  {
-    id: '00142682h1',
-    role: 'admin',
-    email: 'zachary.lavoy@supremelending.com',
-    password: 'adminlavoy',
-    firstName: 'Zachary',
-    lastName: 'LaVoy',
-  },
-  {
-    id: '0019y682h1',
-    role: 'admin',
-    email: 'TJ.Flood@supremelending.com',
-    password: 'adminflood',
-    firstName: 'TJ',
-    lastName: 'Flood',
-  },
-  {
-    id: 'bqcu4',
-    role: 'smgr',
-    email: 'Nick.Yeargin@supremelending.com',
-    password: 'ny6smgr',
-    firstName: 'Nick',
-    lastName: 'Yeargin',
-    accounts: [38, 5, 41, 6, 42, 7, 9, 11, 43, 13, 14, 15, 22, 17, 18, 19, 20, 21, 44, 25]
-  },
-  {
-    id: 'b164bd',
-    role: 'admin',
-    email: 'John.Ragland.Jr@supremelending.com', // not his real email
-    password: 'jr4smgr',
-    firstName: 'John',
-    lastName: 'Ragland',
-    accounts: []
-  },
-  {
-    id: 'tuxb35',
-    role: 'admin',
-    email: 'Brian.Messer@supremelending.com',
-    password: 'bm8smgr',
-    firstName: 'Brian',
-    lastName: 'Messer',
-    accounts: []
-  },
-  {
-    id: 'vs7d',
-    role: 'smgr',
-    email: 'Richard.Lester@supremelending.com',
-    password: 'rl9smgr',
-    firstName: 'Richard',
-    lastName: 'Lester',
-    accounts: [12, 24, 25]
-  },
-  {
-    id: 'inw8c',
-    role: 'admin',
-    email: 'Amir.Davari@supremelending.com', // Not his real Email
-    password: 'ad12smgr',
-    firstName: 'Amir',
-    lastName: 'Davari',
-    accounts: []
-  },
-  {
-    id: 'qvnr61',
-    role: 'admin',
-    email: 'Scott.Mackie@supremelending.com',
-    password: 'sm91smgr',
-    firstName: 'Scott',
-    lastName: 'Mackie',
-    accounts: []
-  },
-  {
-    id: '83hsyc7',
-    role: 'smgr',
-    email: 'Keith.Johnson@supremelending.com',
-    password: 'kj76smgr',
-    firstName: 'Keith',
-    lastName: 'Johnson',
-    accounts: [12, 25]
-  },
-  {
-    id: '74hvs6',
-    role: 'admin',
-    email: 'Robert.Wills@supremelending.com',
-    password: 'rw24smgr',
-    firstName: 'Robert',
-    lastName: 'Wills',
-    accounts: []
-  },
-  {
-    id: 'peb63m',
-    role: 'admin',
-    email: 'Tom.Flood@supremelending.com',
-    password: 'tf16smgr',
-    firstName: 'Tom',
-    lastName: 'Flood',
-    accounts: []
-  },
-  {
-    id: 'y254nw',
-    role: 'admin',
-    email: 'Jeffrey.Sachs@supremelending.com',
-    password: 'js81smgr',
-    firstName: 'Jeffrey',
-    lastName: 'Sachs',
-    accounts: []
-  },
-  {
-    id: 'nsdfuv72hvi',
-    role: 'admin',
-    email: 'Patrick.Flood@supremelending.com',
-    password: 'adminpflood',
-    firstName: 'Pat',
-    lastName: 'Flood'
-  },
-  {
-    id: 'bniw27unv',
-    role: 'smgr',
-    email: 'Cale.Iorg@supremelending.com',
-    password: 'ci46smgr',
-    firstName: 'Cale',
-    lastName: 'Iorg',
-    accounts: [20, 42, 25]
-  }
+  { id: '1', role: 'admin', email: 'jakbellamy@gmail.com', password: 'admin', firstName: 'Jakob', lastName: 'Bellamy', },
+  { id: '00178682h1', role: 'admin', email: 'jmvolpe@nc.rr.com', password: 'adminvolpe', firstName: 'Jeanne', lastName: 'Volpe', },
+  { id: '00142682h1', role: 'admin', email: 'zachary.lavoy@supremelending.com', password: 'adminlavoy', firstName: 'Zachary', lastName: 'LaVoy', },
+  { id: '0019y682h1', role: 'admin', email: 'TJ.Flood@supremelending.com', password: 'adminflood', firstName: 'TJ', lastName: 'Flood', },
+  { id: 'bqcu4', role: 'smgr', email: 'Nick.Yeargin@supremelending.com', password: 'ny6smgr', firstName: 'Nick', lastName: 'Yeargin', accounts: [38, 5, 41, 6, 42, 7, 9, 11, 43, 13, 14, 15, 22, 17, 18, 19, 20, 21, 44, 25] },
+  { id: 'b164bd', role: 'admin', email: 'John.Ragland.Jr@supremelending.com', password: 'jr4smgr',firstName: 'John',lastName: 'Ragland',accounts: [] },
+  { id: 'tuxb35', role: 'admin', email: 'Brian.Messer@supremelending.com', password: 'bm8smgr', firstName: 'Brian', lastName: 'Messer', accounts: [] },
+  { id: 'vs7d', role: 'smgr', email: 'Richard.Lester@supremelending.com', password: 'rl9smgr', firstName: 'Richard', lastName: 'Lester', accounts: [12, 24, 25] },
+  { id: 'inw8c', role: 'admin', email: 'Amir.Davari@supremelending.com',password: 'ad12smgr',firstName: 'Amir',lastName: 'Davari',accounts: []},
+  { id: 'qvnr61', role: 'admin', email: 'Scott.Mackie@supremelending.com', password: 'sm91smgr', firstName: 'Scott', lastName: 'Mackie', accounts: [] },
+  { id: '83hsyc7', role: 'smgr', email: 'Keith.Johnson@supremelending.com', password: 'kj76smgr', firstName: 'Keith', lastName: 'Johnson', accounts: [12, 25] },
+  { id: '74hvs6', role: 'admin', email: 'Robert.Wills@supremelending.com', password: 'rw24smgr', firstName: 'Robert', lastName: 'Wills', accounts: [] },
+  { id: 'peb63m', role: 'admin', email: 'Tom.Flood@supremelending.com', password: 'tf16smgr', firstName: 'Tom', lastName: 'Flood', accounts: [] },
+  { id: 'y254nw', role: 'admin', email: 'Jeffrey.Sachs@supremelending.com', password: 'js81smgr', firstName: 'Jeffrey', lastName: 'Sachs', accounts: [] },
+  { id: 'nsdfuv72hvi', role: 'admin', email: 'Patrick.Flood@supremelending.com', password: 'adminpflood', firstName: 'Pat', lastName: 'Flood' },
+  { id: 'bniw27unv', role: 'smgr', email: 'Cale.Iorg@supremelending.com', password: 'ci46smgr', firstName: 'Cale', lastName: 'Iorg', accounts: [20, 42, 25] }
 ]
 
 
