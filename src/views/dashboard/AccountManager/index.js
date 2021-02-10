33db3173078562a843087a0db98144ddbf51f4dc
Jakob <43860560+jakbellamy@users.noreply.github.com>
2021-02-09 23:14:19 -0500

    Auto Commit

--- src/views/dashboard/AccountManager/index.js
+++ src/views/dashboard/AccountManager/index.js
@@ -1,37 +1,54 @@
+//----------------------------------------------------------
+//Libraries
 import React, { useEffect, useState } from 'react';
-import { Container, Grid, makeStyles } from '@material-ui/core';
+import { Container, Grid } from '@material-ui/core';
+
+//----------------------------------------------------------
+//Modules
 import Page from 'src/components/Page';
+import { DataBox } from './src/Modules/DataBox/DataBox';
+import BoxDivider from './src/Modules/BoxDivider';
+import NullBlock from './src/Modules/NullBlock';
 
 //----------------------------------------------------------
 //Components
-import DataOverview from './src/DataOverview/DataOverview';
-import GraphCard from './src/Modules/GraphCard';
-import NullBlock from './src/Modules/NullBlock';
 import ReportCardHeader from './src/ReportCardHeader';
-import BoxDivider from './src/Modules/BoxDivider';
+import GraphCard from './src/Modules/GraphCard';
+
 //----------------------------------------------------------
 //Functions
 import { reportCardIndexStyles } from './styles';
 import {searchData} from './parsing';
+
 //----------------------------------------------------------
 //Constants
 const data = require('./data.json')
 const ppbData = require('./ppb.json')
 const useStyles = reportCardIndexStyles()
+
+
 //----------------------------------------------------------!
 //                          HOOK
 //----------------------------------------------------------!
+
+
 export default function DashboardAlternativeView(props) {
   const [accountData, setAccountData] = useState([])
+  const [thisMonth, setThisMonth] = useState({})
   const [ppb, setPpb] = useState([{ 'Title': '', 'Date': '' }])
   const [imageSrc, setImageSrc] = useState('')
   const classes = useStyles();
 
   useEffect(() => {
     let params = props.match.params[0] ? props.match.params[0].split('/')[1] : ''
-    setAccountData(searchData(data, params))
+    let _accountData = searchData(data, params)
+    let _thisMonth = _accountData.length > 0 ? _accountData[_accountData.length - 1] : null
+    let _imageSrc = `./Plots/${searchData(data, params)[0]['Account']}-report_card_plot.png`
+
+    setAccountData(_accountData)
+    setThisMonth(_thisMonth)
     setPpb(ppbData)
-    setImageSrc(`./Plots/${searchData(data, params)[0]['Account']}-report_card_plot.png`)
+    setImageSrc(_imageSrc)
   }, []);
 
   return (
@@ -48,11 +65,13 @@ export default function DashboardAlternativeView(props) {
 
         <Grid container spacing={3} id={'Report-Card-Content'}>
           <Grid item xs={6} id={'Data-Column'}>
-            {/*<DataOverview*/}
-            {/*  accountData={accountData}*/}
-            {/*  thisMonth={accountData.length > 0 ? accountData[accountData.length - 1] : null}*/}
-            {/*  key={Math.floor(Math.random() * 101)}*/}
-            {/*/>*/}
+            <DataBox
+              title="Sales Today"
+              value="1.500"
+              differenceValue="+20%"
+              caption=" Since last week"
+            />
+
             <GraphCard imageSrc={imageSrc} height={430} />
           </Grid>
 
