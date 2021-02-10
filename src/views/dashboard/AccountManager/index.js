67bc656270f0de2a6b70e2786c9dfa5c55381164
Jakob <43860560+jakbellamy@users.noreply.github.com>
2021-02-09 22:47:56 -0500

    Auto Commit

--- src/views/dashboard/AccountManager/index.js
+++ src/views/dashboard/AccountManager/index.js
@@ -1,18 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import { Container, Grid, makeStyles } from '@material-ui/core';
 import Page from 'src/components/Page';
+
+//----------------------------------------------------------
+//Components
 import DataOverview from './src/DataOverview/DataOverview';
-import {data, searchData} from './parsing';
-import Divider from '@material-ui/core/Divider';
-import Box from '@material-ui/core/Box';
 import GraphCard from './src/Modules/GraphCard';
 import NullBlock from './src/Modules/NullBlock';
 import ReportCardHeader from './src/ReportCardHeader';
-import { reportCardIndexStyles } from './styles';
 import BoxDivider from './src/Modules/BoxDivider';
+//----------------------------------------------------------
+//Functions
+import { reportCardIndexStyles } from './styles';
+import {searchData} from './parsing';
+//----------------------------------------------------------
+//Constants
+const data = require('./data.json')
 const ppbData = require('./ppb.json')
-
 const useStyles = reportCardIndexStyles()
+
+//----------------------------------------------------------
+//                          HOOK
+//----------------------------------------------------------
 
 export default function DashboardAlternativeView(props) {
   const [accountData, setAccountData] = useState([])
@@ -29,14 +38,14 @@ export default function DashboardAlternativeView(props) {
 
   return (
     <Page className={classes.root} title="Auto Report Card">
-      <Container maxWidth={false} className={classes.container}>
+      <Container maxWidth={false} className={classes.container} id={'content-container'}>
         <NullBlock />
         <ReportCardHeader accountData={accountData} />
         <BoxDivider paddingTop={2} paddingBottom={3} />
 
 
-        <Grid container spacing={3}>
-          <Grid item xs={7}>
+        <Grid container spacing={3} id={'Report-Card-Content'}>
+          <Grid item xs={7} id={'Data-Column'}>
             <DataOverview
               accountData={accountData}
               thisMonth={accountData.length > 0 ? accountData[accountData.length - 1] : null}
@@ -44,7 +53,7 @@ export default function DashboardAlternativeView(props) {
             />
             <GraphCard imageSrc={imageSrc} height={430} />
           </Grid>
-          <Grid item xs={5}>
+          <Grid item xs={5} id={'Marketing-Column'}>
           </Grid>
         </Grid>
       </Container>
