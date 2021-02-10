ef8729f3a4d69189f1f3b9fb80a0ef3e9119dc6e
Jakob <43860560+jakbellamy@users.noreply.github.com>
2021-02-10 11:43:49 -0500

    Auto Commit

--- automator/data_generator.py
+++ automator/data_generator.py
@@ -63,7 +63,7 @@ def search(dataframe, column, like_value):
 print('Fetching Database.')
 
 database = dataset.connect(pg_url)
-table_to_frame = lambda table: pd.DataFrame(list(database[table].find()))
+table_to_frame = lambda table: pd.DataFrame(list(database[table].find())).drop_duplicates()
 
 asa_accounts = table_to_frame('account')
 asa_contracts = table_to_frame('asa_contracts')
@@ -311,7 +311,7 @@ print('JSON File Successfully Saved.')
 def report_card_plot(df):
     try:
         null_prod_mask = df['Office Volume'] > 0
-        rf = df
+        rf = df.loc[null_prod_mask]
         df = (df.loc[null_prod_mask]
                 .set_index(pd.to_datetime(df.loc[null_prod_mask]['Date']))
                 .resample('Q', convention='end')
@@ -321,7 +321,9 @@ def report_card_plot(df):
         rf['Date Number'] = dates.datestr2num(rf['Date'].apply(lambda x: str(x)))
 
         df['Date Number'] = dates.datestr2num(df['Date'].apply(lambda x: str(x)))
+
         df['Market Share Volume'] = df['Market Share Volume'].apply(lambda x: pd.to_numeric(x)).fillna(0)
+        rf['Market Share Volume'] = rf['Market Share Volume'].apply(lambda x: pd.to_numeric(x)).fillna(0)
 
         @pyplot.FuncFormatter
         def fake_dates(x, pos):
@@ -336,27 +338,23 @@ def report_card_plot(df):
             return str(int(y / 1000000)) + 'M'
 
         fig, ax = pyplot.subplots()
-        sns.regplot(x="Date Number", y="Office Volume", color='#544D75', data=rf, ax=ax, order=3, ci=1, scatter_kws={"s": 0}, truncate=False)
-        sns.scatterplot(x="Date Number", y="Office Volume", color='#544D75', ax=ax, data=df, s=20)
-
+        sns.regplot(x="Date Number", y="Office Volume", color='#544D75', data=df, ax=ax, order=6, ci=0, scatter_kws={"s": 20})
 
         ax.xaxis.set_major_formatter(fake_dates)
         ax.yaxis.set_major_formatter(large_currency)
 
         ax2 = pyplot.twinx()
 
-        sns.regplot(x="Date Number", y="Market Share Volume", color='#659ADC', data=rf, ax=ax2, order=3, ci=1, scatter_kws={"s": 0}, truncate=False)
-        sns.scatterplot(x="Date Number", y="Market Share Volume", color='#659ADC', ax=ax2, data=df, s=25)
+        sns.regplot(x="Date Number", y="Market Share Volume", color='#659ADC', data=df, ax=ax2, order=6, ci=0, scatter_kws={"s": 20})
 
         ax2.yaxis.set_major_formatter(precentages)
 
         ax.set_xlabel('Month')
 
-        ax.legend(['Market Share'], loc='upper center',
-                  bbox_to_anchor=(.85, -0.05), ncol=5)
-        ax2.legend(['Office Volume'], loc='upper center',
-                   bbox_to_anchor=(.15, -0.05),fancybox=True,
-                   shadow=True, ncol=5)
+        ax.legend(['Office Volume'], loc='upper center',
+                  bbox_to_anchor=(.15, -0.05), ncol=5)
+        ax2.legend(['Market Share'], loc='upper center',
+                   bbox_to_anchor=(.85, -0.05), ncol=5)
     except:
         print('Failed.')
         print(df)
