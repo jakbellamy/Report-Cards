f38c1cdbdbddc92d225698494ae135f45b824a00
Jakob <43860560+jakbellamy@users.noreply.github.com>
2021-02-23 18:12:25 -0500

    Auto Commit

--- automator/run_automation.sh
+++ automator/run_automation.sh
@@ -1,1 +1,7 @@
-python3 data_generator.py && echo "Generating PDFs" && python3 headless_process.py
+echo "Generating Data"
+python3 data_generator.py
+echo "Generating Plots"
+python3 share_graphs.py
+echo "Generating Report Cards (Headless)"
+python3 headless_process.py
+echo "Complete"
