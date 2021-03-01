987f95ce927ea032db255c697527acc40380b906
Jakob <43860560+jakbellamy@users.noreply.github.com>
2021-03-01 18:29:15 -0500

    Auto Commit

--- automator/data_generator.py
+++ automator/data_generator.py
@@ -1,306 +1,155 @@
-import os
-import dataset
-import numpy as np
 import pandas as pd
+import numpy as np
 from dotenv import load_dotenv
+import os
+from datetime import datetime
+from supreme_data.Database import Database
 from matplotlib import pyplot, dates
 import statsmodels.api as sm
 import seaborn as sns
-from supreme_data.pandas_methods import apply_to_multiple_columns
-from supreme_data.pandas_methods import safe_divide
+from supreme_data import pandas_methods as pm
 
 load_dotenv('../.env')
 
-pg_url = os.getenv('PG_URL')
-gs_id = os.getenv('GS_ID')
-
 pd.set_option('mode.chained_assignment', None)
-# pd.set_option('figure.max_open_warning', None)
-
 sns.set_style("white")
 
-########################################################################################################################
-#                                                   Index                                                              #
-########################################################################################################################
+db = Database(os.getenv('PG_URL'))
 
-"""
-    Step 1:     Fetch Databases Tables and convert to Pandas DataFrame Objects.
-    Step 2:     Coerce Column dTypes to avoid errors.
-    Step 3:     Filter Tables and coerce naming convention.
-    Step 4:     Update Tables via Merges, Data Pulls or Other Munging.
-    Step 4:     Data Correction, Normalization.
-    Step 5.     Perform DataFrame Calculations.
-    Step 6.     Create JSON File for Dash/Report Card.
-    Step 7.     Plot Graphs and Each as 300dpi PNGs in Public Folder for Report Card Retrieval.
-"""
+asa_accounts = db.fetch_table('asa_accounts')
+asa_contracts = db.fetch_table('asa_contracts')
+asa_lites = db.fetch_table('lite_accounts')
+lite_contracts = db.fetch_table('lite_contracts')
+asa_production = db.fetch_table('asa_production')
+lite_production = db.fetch_table('lite_referrals')
 
-########################################################################################################################
-########################################################################################################################
-#                                                   Methods                                                            #
-########################################################################################################################
+def contracts_to_costs(contracts):
+    contracts['End Date'] = contracts['End Date'].apply(lambda x: datetime.now() if pd.isnull(x) else x)
+    costs = []
+    for _,contract in contracts.iterrows():
+        date_range = pd.date_range(start=contract['Effective Date'].replace(day=1),
+                                   end=contract['End Date'].replace(day=1),
+                                   freq='m')
+        for month in date_range:
+            cost_row = (contract['Account'],
+                        month.replace(day=1),
+                        contract['Lead Cost'],
+                        contract['Lease Cost'])
+            costs += [cost_row]
 
-def search(dataframe, column, like_value):
-    """
-    Takes :: a Data Frame, Column and a substring.
-    Returns :: Data Frame filtered for substring in column value.
-    """
-    mask = dataframe[column].apply(lambda x: str(like_value).lower() in str(x).lower())
-    return dataframe[mask]
+    return pd.DataFrame(data=costs,
+                        columns=['Account', 'Date',
+                                'Lead Cost', 'Lease Cost'])
+
 
 ########################################################################################################################
 ########################################################################################################################
 #                                                Script Start                                                          #
 ########################################################################################################################
 
-
-
-                            ####################################################
-                            #                    STEP 1.                       #
-                            ####################################################
-
-
-
-print('Fetching Database.')
-
-database = dataset.connect(pg_url)
-table_to_frame = lambda table: pd.DataFrame(list(database[table].find())).drop_duplicates()
-
-asa_accounts = table_to_frame('account')
-asa_contracts = table_to_frame('asa_contracts')
-asa_lites = table_to_frame('asa_lites')
-lite_contracts = table_to_frame('lite_contracts')
-asa_production = table_to_frame('asa_production')
-lite_production = table_to_frame('referrals')
-ce_events = table_to_frame('continuing_education')
-
-print('Database Fetched.')
-
-                            ####################################################
-                            #                    STEP 2.                       #
-                            ####################################################
-
-
-print('Normalizing Data.')
-
-            ######################
-            #    Date Coercion   #
-
-table_dates = [
-    (asa_accounts, ['Date', 'Start Date', 'Renewal']),
-    (asa_lites, ['Origin Date', 'Termination Date']),
-    (lite_contracts, ['Effective Date', 'End Date']),
-    (asa_contracts, ['Effective Date', 'End Date']),
-    (lite_production, ['Funded Month']),
-    (asa_production, ['date'])
-]
-
-column_as_date = lambda x: pd.to_datetime(x, errors='coerce')
-
-for table, columns in table_dates:
-    apply_to_multiple_columns(table, columns, operation=column_as_date)
-
-
-            #########################
-            #    Numeric Coercion   #
-
-asa_production_pedigree = ['date', 'Account', 'Date Started', 'Agent Count']
-asa_production_numeric = [i for i in list(asa_production.columns) if not i in asa_production_pedigree]
-
-table_numbers = [
-    (asa_production, asa_production_numeric),
-    (asa_contracts, ['Lead Cost', 'Lease Cost']),
-    (lite_contracts, ['Lead Cost', 'Lease Cost'])
-]
-
-columns_as_numeric = lambda x: pd.to_numeric(x, errors='coerce')
-
-for table, columns in table_numbers:
-    apply_to_multiple_columns(table, columns, operation=columns_as_numeric)
-
-
-
-                            ####################################################
-                            #                    STEP 3.                       #
-                            ####################################################
-
-
-
-            ########################
-            #    Table Filtering   #
-
-asa_accounts = asa_accounts.loc[asa_accounts['Active']]
-
-asa_pedigree = ['Name', 'Start Date', 'Termination Date', 'Renewal', 'Agent Count','Loan Officer Count', 'Count of Other Lenders']
-asa_accounts = asa_accounts[asa_pedigree]
-
-asa_production.rename(columns={'date': 'Date', 'Agents': 'Agent Count'}, inplace=True)
-
-asa_lites['ASA'] = asa_lites['ASA'].apply(lambda x: x if x else None)
-
-
-
-                            ####################################################
-                            #                    STEP 4.                       #
-                            ####################################################
-
-
-
-
-            ############################
-            #    Update Agent Counts   #
-
-print('Updating Agent Counts.')
-
-updated_agent_counts = asa_production.dropna(subset=['Agent Count'])
-
-null_agent_mask = updated_agent_counts['Agent Count'] >= 1
-updated_agent_counts = updated_agent_counts.loc[null_agent_mask][['Date', 'Account', 'Agent Count']]
-
-for account, df in updated_agent_counts.groupby('Account'):
-    asa_accounts.set_index('Name').loc[account, 'Agent Count'] = df['Date'].max()
-
-
-            ###############################
-            #    Join CE and ASA Tables   #
-
-print('Adding CE Events')
-
-ce_events = ce_events.loc[ce_events['Date'] > pd.to_datetime('2019-12-31')]
-get_ce_count = lambda x: len(ce_events.groupby('Account').get_group(x)) if x in set(ce_events['Account']) else 0
-asa_accounts['2020 CE Count'] = asa_accounts['Name'].apply(lambda x: get_ce_count(x))
-
-
-            #####################################
-            #    Remove Production after Term   #
-
-print('Removing Production Data Attributed to Terminated Accounts.')
-
-date_ceiling = lambda df_column, date: df_column <= pd.to_datetime(date)
-
-def last_asa_production(asa):
-    return max(list(asa_production.set_index('Account').loc[asa]['Date']))
-
-detailed_lite_production = asa_lites.set_index('ASA Lite').join(lite_production.set_index('Referral Source'))
-detailed_lite_production = detailed_lite_production.dropna(subset=['ASA']).reset_index().rename(columns={'index': 'Account'})
-
-production_after_termination = np.where(detailed_lite_production['Termination Date'],
-                            detailed_lite_production['Termination Date'] < detailed_lite_production['Funded Month'],
-                            False)
-
-out_of_bounds_production = list(detailed_lite_production.loc[production_after_termination].index)
-
-detailed_lite_production.drop(out_of_bounds_production, inplace=True)
-
-
-            #####################################################
-            #    Join ASA Lite Production with ASA Production   #
-
-print('Compiling ASA Lite Data Associated With Each ASA')
-
-lite_production_by_asa = detailed_lite_production.groupby(['ASA', 'Funded Month']).agg('sum')[['Units', 'Total Loan Amount']]
-lite_production_by_asa = lite_production_by_asa.reset_index().rename(columns={
-                                                                        'Units': 'Lite Units',
-                                                                        'Total Loan Amount': 'Lite Volume',
-                                                                        'ASA': 'Account',
-                                                                        'Funded Month': 'Date'
-                                                                    })
-
-asa_production_with_lites = pd.merge(asa_production, lite_production_by_asa, on=['Account', 'Date'], how='left').fillna(0)
-asa_production_with_lites = asa_production_with_lites.drop(columns=['Date Started', 'Agent Count'])
-
-df = asa_production_with_lites.sort_values(by=['Account', 'Date'])
-
-
-
-                            ####################################################
-                            #                    STEP 5.                       #
-                            ####################################################
-
-
-
-            #######################################################
-            #    Aggregate Production into Monthly Company Sums   #
-
-print('Calculating Monthly Company Totals.')
-
-df = df[df['Account'] != 'Monthly Totals']
-df = df[df['Account'] != 'YTD Totals']
+associated_lite_costs = (contracts_to_costs(lite_contracts)
+                          .set_index('Account')
+                          .join(asa_lites.set_index('Account')['Associated ASA'])
+                          .rename(columns={
+                              'Associated ASA': "Account",
+                              "Lead Cost": 'Lite Lead Cost',
+                              "Lease Cost": "Lite Lease Cost"
+                          })
+                          .set_index(["Account", 'Date'], drop=True))
+
+total_asa_costs = (contracts_to_costs(asa_contracts)
+                   .set_index(["Account", 'Date'])
+                   .join(associated_lite_costs)
+                   .fillna(0))
+
+pedigree_columns = list(asa_accounts.set_index('Account').columns)
+
+asa_report = (total_asa_costs
+              .reset_index()
+              .set_index('Account')
+              .join(asa_accounts.set_index('Account'))
+              .set_index(pedigree_columns, append=True)
+              .reset_index()
+              .set_index(['Account', 'Date'])
+              .join(asa_production
+                      .set_index(['Account', 'Date']))
+              .set_index(pedigree_columns, append=True)
+              .fillna(0)
+              .apply(pd.to_numeric)
+              .replace([None], 0)
+              .reset_index()
+              .fillna(''))
+
+
+simplify_loan_purpose = lambda x: x if x == 'Purchase' else 'Non-Purchase'
+lite_production['Loan Purpose'] = lite_production['Loan Purpose'].apply(simplify_loan_purpose)
+
+paired_lite_production = (lite_production
+                          .rename(columns={'Referral Source': 'Account',
+                                          'Funded Month': 'Date'})
+                          .set_index('Account')
+                          .join(asa_lites.set_index('Account')['Associated ASA'])
+                          .reset_index(drop=True)
+                          .rename(columns={'Associated ASA': 'Account'})
+                          .groupby(['Account', 'Date', 'Loan Purpose'])
+                          .agg('sum')[['Units', 'Total Loan Amount']]
+                          .reset_index())
+
+paired_lite_production = (paired_lite_production
+                          .set_index(['Account', 'Date', 'Loan Purpose'])
+                          .unstack())
+
+merged_column_levels = zip(paired_lite_production.columns.get_level_values(1),
+                           paired_lite_production.columns.get_level_values(0))
+
+join_column_levels = lambda x: 'Lites ' + ' '.join(list(x))
+modified_columns = [join_column_levels(column_row) for column_row in merged_column_levels]
+
+paired_lite_production.columns = modified_columns
+
+df = (asa_report
+      .set_index(['Account', 'Date'])
+      .join(paired_lite_production)
+      .reset_index()
+      .sort_values(by=['Date', 'Account'])
+      .fillna(0))
+
+df = df.set_index(pedigree_columns + ['Account']).fillna(0).apply(pd.to_numeric).reset_index().fillna('')
+
+df['Year'] = df['Date'].apply(lambda x: pd.to_datetime(x).year)
+
+def cum_ytd(col):
+    df['YTD ' + col] = df.groupby(['Account', 'Year'])[col].cumsum()
+
+cum_ytd_cols = ['Supreme Volume', 'Office Volume', 'Supreme Units', 'Office Units']
+
+for col in cum_ytd_cols:
+    cum_ytd(col)
 
 totals = df.groupby('Date').agg('sum').reset_index()
 totals['Account'] = 'Company Totals'
 
 df = pd.concat([df, totals], axis=0, ignore_index=True)
 
+df['Market Share Volume'] = pm.safe_divide(df, ['Supreme Volume', 'Office Volume'])
+df['Market Share Units'] = pm.safe_divide(df, ['Supreme Units', 'Office Units'])
+df['Avg Supreme Loan'] = pm.safe_divide(df, ['Supreme Volume', 'Supreme Units'])
+df['Avg Office Loan'] = pm.safe_divide(df, ['Office Volume', 'Office Units'])
+df['YTD Market Share Volume'] = pm.safe_divide(df, ['YTD Supreme Volume', 'YTD Office Volume'])
+df['YTD Market Share Units'] = pm.safe_divide(df, ['YTD Supreme Units', 'YTD Office Units'])
 
-            ################################
-            #    Get Cumulative YTD Sums   #
-
-print('Performing YTD Cumulative Sum Calculations.')
-
-df['Year'] = df['Date'].apply(lambda x: x.year)
-
-def cum_ytd(col):
-    df['YTD ' + col] = df.groupby(['Account', 'Year'])[col].cumsum()
-
-cum_ytd_cols = ['Supreme Volume', 'Office Volume', 'Supreme Units', 'Office Units', 'Lite Volume', 'Lite Units']
-
-for col in cum_ytd_cols:
-    cum_ytd(col)
-
-            ###############################
-            #    Calculate Market Share   #
-
-print('Ensuring Market Shares are Correctly Calculated.')
-
-df['Market Share Volume'] = safe_divide(df, ['Supreme Volume', 'Office Volume'])
-df['YTD Market Share Volume'] = safe_divide(df, ['YTD Supreme Volume', 'YTD Office Volume'])
-df['Market Share Units'] = safe_divide(df, ['Supreme Units', 'Office Units'])
-df['YTD Market Share Units'] = safe_divide(df, ['YTD Supreme Units', 'YTD Office Units'])
-df['Lite Share Volume'] = safe_divide(df, ['Lite Volume', 'Supreme Volume'])
-df['YTD Lite Share Volume'] = safe_divide(df, ['YTD Lite Volume', 'YTD Supreme Volume'])
-df['Lite Share Units'] = safe_divide(df, ['Lite Units', 'Supreme Units'])
-df['YTD Lite Share Units'] = safe_divide(df, ['YTD Lite Units', 'YTD Supreme Units'])
-df = df.fillna(0)
-
-
-
-                            ####################################################
-                            #                    STEP 6.                       #
-                            ####################################################
-
-
-
-print('Saving Compiled Data to JSON File.')
-
-            ###################################
-            # Drop Null Data Hiding as Zeroes #
-
-null_data_mask = df['Office Volume'] >= 0
+null_data_mask = df['Office Volume'] > 0
 df = df.loc[null_data_mask]
-
-            #################################
-            #    Convert Dates to Strings   #
+df = df.fillna('')
 
 df['Date Time'] = df['Date']
-df['Date'] = df['Date'].apply(lambda x: x.strftime('%B %Y'))
+df['Date'] = df['Date'].apply(lambda x: pd.to_datetime(x).strftime('%B %Y'))
+df['Renewal'] = df['Renewal'].apply(lambda x: x if isinstance(x, str) else pd.to_datetime(x).strftime('%B %d, %Y'))
+df['Start Date'] = df['Start Date'].apply(lambda x: x if isinstance(x, str) else pd.to_datetime(x).strftime('%B %d, %Y'))
 
-date_to_text = lambda x: x if isinstance(x, str) else x.strftime('%B %d, %Y')
-date_cols = ['Termination Date', 'Renewal', 'Start Date']
+# date_cols = ['Termination Date', 'Renewal', 'Start Date']
+# df[date_cols] = df[date_cols].apply(lambda x: x if isinstance(x, str) else pd.to_datetime(x).strftime('%B %d, %Y'))
 
-asa_accounts[date_cols] = asa_accounts[date_cols].fillna('').apply(date_to_text)
-
-
-            ########################################################
-            #    Save DataFrame as JSON to Report Card Directory   #
-
-monthly_data = pd.merge(df, asa_accounts, left_on=['Account'], right_on=['Name'], how='left').fillna(0).drop(columns=['Name'])
-name_key = {
-    'KW - Brandon': 'KW - Suburban Tampa',
-    'KW - Kannapolis': 'KW - Premier',
-    'KW - Athens': 'KW - Greater Athens'
-}
-apply_name_key = lambda account: name_key[account] if account in list(name_key.keys()) else account
-monthly_data['Account'] = monthly_data['Account'].apply(apply_name_key)
-monthly_data.to_json('../src/Report Card/data/data.json', orient='records')
+df.to_json('../src/Report Card/data/data.json', orient='records')
 
 print('JSON File Successfully Saved.')
